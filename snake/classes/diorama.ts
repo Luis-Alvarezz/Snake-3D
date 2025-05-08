e927045e027068dd1cc88c0d9b6ddc3159c0e0a9
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-07 22:39:41 -0600

    [MODIFY]: Se agrego la instancia de la clase Ground

--- snake/classes/diorama.ts
+++ snake/classes/diorama.ts
@@ -1,14 +1,17 @@
 import { LifeCycle } from "../types/helpers";
 import Food from "./food";
 import Snake from './snake';
+import Ground from "./ground";
 
 export default class Diorama implements LifeCycle {
     private snake: Snake;
     private food: Food;
+    private ground: Ground;
 
     public start(): void {
         this.snake = new Snake();
         this.food = new Food();
+        this.ground = new Ground();
     }
 
     public update(): void {
