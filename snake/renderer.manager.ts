4b5e51f4ce5c4d7c75e3377b908d290f95c8bae9
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-07 22:45:27 -0600

    [ADDED]: Metodo para post procesado en luces, se inicializa en metodo init y se genera en ciclo en metodo renderLoop

--- snake/renderer.manager.ts
+++ snake/renderer.manager.ts
@@ -1,10 +1,15 @@
-import { RenderTargetArray, WebGLRenderer } from "three";
+import { Vector2, WebGLRenderer } from "three";
 import SceneManager from "./scene.manager";
-
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 export default class RendererManager {
     // * Clase encargada de renderer, post procesado, redimensionamiento de la pantalla, singleton (instaciar 1 vez unicamente la clase principal)
     private static renderer: WebGLRenderer;
     public static canvas: HTMLCanvasElement;
+    private static composer: EffectComposer;
+    private static renderPass: RenderPass;
+    private static bloomPass: UnrealBloomPass;
 
     private constructor() {
         // console.log('Numero de instancia 1');
@@ -17,6 +22,7 @@ export default class RendererManager {
     private static init() : void {
         RendererManager.getCanvas();
         RendererManager.createRenderer();
+        RendererManager.postProcessing();
     }
 
     private static getCanvas() : void {
@@ -34,6 +40,7 @@ export default class RendererManager {
     private static renderLoop(): void {
         requestAnimationFrame(RendererManager.renderLoop);
         RendererManager.renderer.render(SceneManager.scene, SceneManager.camera);
+        RendererManager.composer.render();
     }
 
 
@@ -50,5 +57,22 @@ export default class RendererManager {
         if (!RendererManager.renderer) {
             new RendererManager();
         }
+    }
+
+
+    private static postProcessing() : void {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const renderScene = new RenderPass(SceneManager.scene, SceneManager.camera);
+        const bloomPass = new UnrealBloomPass(new Vector2(width, height), 1.5, 0.4, 0.85);
+        bloomPass.threshold = 0;
+        bloomPass.strength = 1; // * Intensidad del efecto
+        bloomPass.radius = 0;
+        bloomPass.renderToScreen = true;
+        RendererManager.composer = new EffectComposer(RendererManager.renderer);
+        RendererManager.composer.setSize(width, height);
+        RendererManager.composer.addPass(renderScene);
+        RendererManager.composer.addPass(bloomPass);
+        RendererManager.renderer.toneMappingExposure = Math.pow(0.9, 4.0);
     }
 }
