3fbd4ef6688041106d106dddf8649cb7cbe7c7fb
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-13 22:24:13 -0600

    [ADDED]: Se añadio componente Panel al render principal

--- snake/index.tsx
+++ snake/index.tsx
@@ -1,6 +1,7 @@
 import { Component, ReactNode } from "react";
 import RendererManager from "./renderer.manager";
 import { Menu } from "./ui/menu";
+import Panel from "./ui/panel";
 
 export default class SnakeComponent extends Component {
 
@@ -12,6 +13,7 @@ export default class SnakeComponent extends Component {
         return (
             <div>
                 <canvas id="game" className="fullscreen"></canvas>
+                <Panel />
                 <Menu />
             </div>
         )
