f63fd2bce756cc8daf95ed5e3bedac7d55fbac78
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-13 21:09:37 -0600

    [MODIFY]: Se añadio la instancia de la clase de creditos a los estados globales

--- snake/states/states.ts
+++ snake/states/states.ts
@@ -1,6 +1,8 @@
+import CreditState from "./creditos.state";
 import MenuState from "./menu.state";
 
 export default class States {
     // * Clase para crear instancia de los estados
-    public static menu = new MenuState();
+    public static menu = new MenuState;
+    public static credit = new CreditState;
 }
