68d774075e65fdb42dea3c4b77e327f80d881224
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-14 18:52:04 -0600

    [MODIFY]: Se añadio la instancia de la nueva clase GameState

--- snake/states/states.ts
+++ snake/states/states.ts
@@ -1,8 +1,10 @@
 import CreditState from "./creditos.state";
+import GameState from "./game.state";
 import MenuState from "./menu.state";
 
 export default class States {
     // * Clase para crear instancia de los estados
     public static menu = new MenuState;
     public static credit = new CreditState;
+    public static game = new GameState;
 }
